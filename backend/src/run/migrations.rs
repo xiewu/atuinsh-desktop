ab6c73abc1d319e27d274aa6ca3521f6ecd8197d
Ellie Huxtable <ellie@elliehuxtable.com>
2024-09-24 18:00:47 -0700

    chore(release): bump version

--- backend/src/run/migrations.rs
+++ backend/src/run/migrations.rs
@@ -7,6 +7,18 @@ pub fn migrations() -> Vec<Migration> {
             description: "create_initial_tables",
             sql: "CREATE TABLE runbooks(id string PRIMARY KEY, name TEXT, content TEXT, created bigint, updated bigint);",
             kind: MigrationKind::Up,
+        },
+        Migration {
+            version: 2,
+            description: "create_workspace_table",
+            sql: "create table workspaces(id string primary key, name text, created bigint, updated bigint);",
+            kind: MigrationKind::Up,
+        },
+        Migration {
+            version: 3,
+            description: "add_workspace_id_to_runbooks",
+            sql: "alter table runbooks add column workspace_id string;",
+            kind: MigrationKind::Up,
         }
     ]
 }

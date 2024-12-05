26fc43bdd399dcb654cff1d9ce9381dbdaf781b9
Michelle Tilley <michelle@michelletilley.net>
2024-12-05 15:22:37 -0800

    Only listen for resynced once

--- src/components/runbooks/editor/Editor.tsx
+++ src/components/runbooks/editor/Editor.tsx
@@ -215,7 +215,7 @@ export default function Editor() {
         },
       });
 
-      provider.on("synced", () => {
+      provider.once("synced", () => {
         // If the loaded YJS dot has no content, and the server has no content,
         // we should take the old `content` field (if any) and populate the editor
         // so that we trigger a save, creating the YJS document.

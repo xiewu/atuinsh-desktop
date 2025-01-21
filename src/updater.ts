036cfb81f1f14b00a9473be63a855ca8f8819b05
Michelle Tilley <michelle@michelletilley.net>
2025-01-21 09:45:47 -0800

    Suppress update prompt in dev mode (#325)

--- src/updater.ts
+++ src/updater.ts
@@ -2,13 +2,14 @@ import { check } from "@tauri-apps/plugin-updater";
 import { ask } from "@tauri-apps/plugin-dialog";
 import { relaunch } from "@tauri-apps/plugin-process";
 import Logger from "@/lib/logger";
+import AtuinEnv from "./atuin_env";
 const logger = new Logger("Updater");
 
 export async function checkForAppUpdates(): Promise<boolean> {
   logger.debug("Checking for updates...");
   const update = await check();
 
-  if (update?.available) {
+  if (update?.available && AtuinEnv.isProd) {
     logger.info("Update available!");
 
     const yes = await ask(
@@ -27,6 +28,8 @@ ${update.version} is available!
       await update.downloadAndInstall();
       await relaunch();
     }
+  } else if (update?.available) {
+    logger.info("Update available; suppressing prompt in development mode")
   }
 
   return update?.available ?? false;

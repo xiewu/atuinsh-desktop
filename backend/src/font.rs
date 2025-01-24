e3b94e67f15fa18db0710ad97bd6cbfd9f6c4ad9
Ellie Huxtable <ellie@atuin.sh>
2025-01-24 21:52:54 +0000

    fix: really stupid error (#352)
    * fix: really stupid error
    
    * i should have my rust license revoked

--- backend/src/font.rs
+++ backend/src/font.rs
@@ -1,5 +1,4 @@
 use font_kit::{error::SelectionError, source::SystemSource};
-use std::vec;
 
 #[tauri::command]
 pub async fn list_fonts() -> Vec<std::string::String> {
@@ -9,12 +8,5 @@ pub async fn list_fonts() -> Vec<std::string::String> {
     // Get all fonts in the system
     let fonts: Result<Vec<String>, SelectionError> = source.all_families();
 
-    //return font
-    if let Ok(font) = fonts {
-        font
-    } else {
-        vec![]
-    }
-
-    font.unwrap_or_default()
+    fonts.unwrap_or_default()
 }

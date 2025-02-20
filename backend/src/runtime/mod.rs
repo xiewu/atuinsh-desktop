d99ace7e5611b23eb70e4cb95a1e2e26975c6a06
Ellie Huxtable <ellie@elliehuxtable.com>
2025-02-19 17:03:43 -0800

    chore(release): bump version

---
+++ backend/src/runtime/mod.rs
@@ -0,0 +1,2 @@
+pub(crate) mod runbook;
+pub(crate) mod ssh;
---
+++ backend/src/runtime/runbook.rs
@@ -0,0 +1,4 @@
+// Track the runtime state for a runbook
+// Currently this struct only holds SSH connections, but will later be expanded to include other runtime state
+
+pub(crate) struct RunbookRuntime {}

6f6a325c856cd484ca27c0d4015432cbf58f9262
Michelle Tilley <michelle@michelletilley.net>
2025-03-10 13:19:57 -0700

    Add helpers to sqlite module for opening a database

--- backend/src/sqlite.rs
+++ backend/src/sqlite.rs
@@ -1,4 +1,7 @@
-use sqlx::{migrate::MigrateDatabase, Pool, Sqlite, SqlitePool};
+use std::{path::PathBuf, str::FromStr, time::Duration};
+
+use eyre::Result;
+use sqlx::{migrate::MigrateDatabase, sqlite, Pool, Sqlite, SqlitePool};
 use tauri::{AppHandle, Manager};
 use tokio::fs::create_dir_all;
 
@@ -28,4 +31,54 @@ pub(crate) async fn connect(database: &str, app: &AppHandle) -> Result<Pool<Sqli
     SqlitePool::connect(conn_url)
         .await
         .map_err(|e| e.to_string())
+}
+
+pub(crate) fn db_file(app: &AppHandle, database: &str) -> String {
+    let state = app.state::<crate::state::AtuinState>();
+    let dev_prefix = state.dev_prefix.as_ref();
+
+    let ext = if database.ends_with(".db") { "" } else { ".db" };
+
+    dev_prefix.map_or(format!("{}{}", database, ext), |prefix| {
+        format!("{}_{}{}", prefix, database, ext)
+    })
+}
+
+pub(crate) fn db_path(app: &AppHandle, database: &str) -> PathBuf {
+    let app_path = app
+        .path()
+        .app_config_dir()
+        .expect("Failed to get app config dir");
+
+    app_path.join(db_file(app, database))
+}
+
+/// Get a pool for the given database. If the database does not exist, it will be created.
+/// Database files are stored in the app config directory (`app.path().app_config_dir()`)
+/// to match the behavior of `tauri-plugin-sql`.
+///
+/// If the database name does not end in `.db`, it will be appended automatically.
+pub(crate) async fn get_pool(app: &AppHandle, database: &str) -> Result<Pool<Sqlite>> {
+    let db_path = db_path(app, database);
+
+    let create = !db_path.exists();
+    if create {
+        if let Some(dir) = db_path.parent() {
+            create_dir_all(dir).await?;
+        }
+    }
+
+    let opts = sqlite::SqliteConnectOptions::from_str(db_path.to_str().unwrap())?
+        .journal_mode(sqlite::SqliteJournalMode::Wal)
+        .optimize_on_close(true, None)
+        .synchronous(sqlite::SqliteSynchronous::Normal)
+        .with_regexp()
+        .create_if_missing(true);
+
+    let pool = sqlite::SqlitePoolOptions::new()
+        .acquire_timeout(Duration::from_secs_f64(3.0))
+        .connect_with(opts)
+        .await?;
+
+    Ok(pool)
 }

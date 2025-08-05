c8730cdc981edf379d5f43889a45434634fdd74d
Michelle Tilley <michelle@michelletilley.net>
2025-08-05 12:26:17 -0700

    chore: Add ts-rs (#576)
    * Add ts-rs
    
    * Make ts-stdlib available everywhere

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,15 @@
 declare global {
   type Timeout = ReturnType<typeof setTimeout>;
   type Interval = ReturnType<typeof setInterval>;
+
+  type Option<T> = import("@binarymuse/ts-stdlib").Option<T>;
+  type Result<T, E> = import("@binarymuse/ts-stdlib").Result<T, E>;
+
+  var Some = import("@binarymuse/ts-stdlib").Some;
+  var None = import("@binarymuse/ts-stdlib").None;
+  var Ok = import("@binarymuse/ts-stdlib").Ok;
+  var Err = import("@binarymuse/ts-stdlib").Err;
 }
 
-export { Timeout, Interval };
+// This export is required to make this a module
+export {};

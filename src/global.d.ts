5a31c9764f557a3a9796c82967116986528d9e00
Michelle Tilley <michelle@michelletilley.net>
2025-08-05 10:00:13 -0700

    Make ts-stdlib available everywhere

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,15 @@
 declare global {
   type Timeout = ReturnType<typeof setTimeout>;
   type Interval = ReturnType<typeof setInterval>;
+
+  type Option<T> = import("@binarymuse/ts-stdlib").Option<T>;
+  type Result<T, E> = import("@binarymuse/ts-stdlib").Result<T, E>;
+
+  var Some: <T>(value: T) => import("@binarymuse/ts-stdlib").Option<T>;
+  var None: import("@binarymuse/ts-stdlib").Option<any>;
+  var Ok: <T>(value: T) => import("@binarymuse/ts-stdlib").Result<T, never>;
+  var Err: <E>(error: E) => import("@binarymuse/ts-stdlib").Result<never, E>;
 }
 
-export { Timeout, Interval };
+// This export is required to make this a module
+export {};

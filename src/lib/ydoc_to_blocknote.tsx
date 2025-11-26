dafcac9d53a5f7cb3107a29e6e091f09fc775960
Michelle Tilley <michelle@michelletilley.net>
2025-11-25 17:58:38 -0800

    Revert Y.Doc to BlockNote conversion strategy (#251)

--- src/lib/ydoc_to_blocknote.tsx
+++ src/lib/ydoc_to_blocknote.tsx
@@ -1,17 +1,40 @@
 import * as Y from "yjs";
+import debounce from "lodash.debounce";
 import { createConversionEditor } from "@/components/runbooks/editor/create_editor";
 import { Block } from "@blocknote/core";
 import Mutex from "./std/mutex";
-import { yDocToBlocks } from "@blocknote/core/yjs";
 
 let convertMutex = new Mutex();
 
 export async function ydocToBlocknote(doc: Y.Doc): Promise<Block<any>[]> {
   const unlock = await convertMutex.lock();
 
-  const editor = createConversionEditor(doc, doc.getXmlFragment("document-store"));
-  const blocks = yDocToBlocks(editor, doc, "document-store");
+  const promise = new Promise<Block<any>[]>((resolve, reject) => {
+    let resolved = false;
 
-  unlock();
-  return blocks as Block<any>[];
+    const fragment = doc.getXmlFragment("document-store");
+    const editor = createConversionEditor(doc, fragment);
+
+    editor.onChange(
+      debounce((editor) => {
+        if (resolved) return;
+        resolved = true;
+        resolve(editor.document);
+        editor.unmount();
+      }, 100),
+    );
+
+    const el = document.createElement("div");
+    setTimeout(() => {
+      if (resolved) return;
+      resolved = true;
+      reject(new Error("Yjs to BlockNote conversation timed out"));
+      editor.unmount();
+    }, 5000);
+
+    editor.mount(el);
+  });
+
+  promise.finally(() => unlock());
+  return promise;
 }
